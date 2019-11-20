1bbdd154b75a79b654dc1a43fcc2b1b774b38d50
Tochma <tochmalia@gmail.com>
2019-11-20 12:30:07 +0900

    설명 추가

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+"""
+사전에 만든 각종 파일을 import 해서 사용
+Main 함수는 '사람을 찾는' 기능을 하는 함수이다.
+Main 이전에 사용자의 id와 pw를 받아, 여러 번 id, pw 를 받을 필요가 없도록 한다.
+"""
+print("Welcome! to our Program")
+print("Made by 'Saram' in 2019")
+print("="*20)
+print("SASA 학생의 위치를 쉽게 찾아주는 프로그램입니다.")
+print("="*20)
+
 def Main():
     pass
 
