7808f38c4f4be98dee8125f2fcd8794b45b24c26
Tochma <tochmalia@gmail.com>
2019-11-20 12:00:42 +0900

    Sort_Time 의 조건을 새로 추가했고, 시간을 계산하는 ㅏ코드를 작성중 (미완성)

--- Sorting Time_LSI.py
+++ Sorting Time_LSI.py
@@ -2,9 +2,13 @@ import datetime
 import locale
 
 
-def Sort_Time(year=2019, month=None, day=None, day_name=None, hour=None, minute=None):
+def Sort_Time(year=2019, month=None, day=None, day_name=None, hour=None, minute=None, time_name=None):
     """
     모든 변수를 다 사용할 필요는 없다.
+    년도는 2019가 기본. 요일 혹은 날짜를 기입해야 한다.
+    시간, 혹은 시간표상에서 몇 교시인지 기입한다.
+    점호 마감 전이면 0, 8시부터 1교시 시작 전은 0.5
+    모든 교시는 정수이며, 그 사이 쉬는시간은 바로 앞 교시에 0.5를 더하여 표기한다.
     :param year: 년도, int 타입 지향, string 타입 지원
     :param month: 월, int 타입 지향, string 타입 지원
     :param day: 일, int 타입 지향, string 타입 지원
@@ -12,6 +16,7 @@ def Sort_Time(year=2019, month=None, day=None, day_name=None, hour=None, minute=
     "금", "토", "월" 과 같은 형태로 입력
     :param hour: 시 (24시간 방법 사용!), int 타입 지향, string 타입 지원
     :param minute: 분, int 타입 지향, string 타입 지원
+    :param time_name:
     :return: 보아야 하는 시간표를 [ , ] 형태 리스트로 반환한다. 이 순서는 요일 - 시간 순이다.
     """
 
@@ -36,6 +41,22 @@ def Sort_Time(year=2019, month=None, day=None, day_name=None, hour=None, minute=
 
     hour = int(hour)
     minute = int(minute)
+    time = 100*hour + minute
+
+    if time < 840:
+        answer.append(0)
+    elif time <= 930:
+        answer.append(1)
+    elif time < 940:
+        answer.append(1.5)
+    elif time <= 1030:
+        answer.append(2)
+    elif time < 1040:
+        answer.append(2.5)
+    elif time <= 1130:
+        answer.append(3)
+    elif time < 1140:
+        pass
 
 
 if __name__ == "__main__":
