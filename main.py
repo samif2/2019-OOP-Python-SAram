7403812cdeb81382f6861824fe653867e9b637cc
Tochma <tochmalia@gmail.com>
2019-11-26 10:52:47 +0900

    main 함수 작업중: 이름 입력

--- main.py
+++ main.py
@@ -3,14 +3,23 @@
 Main 함수는 '사람을 찾는' 기능을 하는 함수이다.
 Main 이전에 사용자의 id와 pw를 받아, 여러 번 id, pw 를 받을 필요가 없도록 한다.
 """
+
+import SortingTime_LSI
+import SortingTime_LSI
+
 print("Welcome! to our Program")
 print("Made by 'Saram' in 2019")
-print("="*20)
+print("=" * 20)
 print("SASA 학생의 위치를 쉽게 찾아주는 프로그램입니다.")
-print("="*20)
+print("=" * 20)
+
 
 def Main():
-    pass
+    print("찾고자 하는 학생의 이름을 입력하세요.")
+    student_name = input(">")
+
+    # Web_LJS 실행
+    # 만일 그런 사람이 없다면, 모든 과정을 생략하고 다시 할 것인지 묻는 부분으로 간다.
 
 
 check_Main = True
