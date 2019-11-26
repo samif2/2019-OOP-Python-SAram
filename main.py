9d746df6177c89e9e050e910e318e7bd7787c8f8
Tochma <tochmalia@gmail.com>
2019-11-26 11:18:00 +0900

    main 함수 작업중: 시간 입력

--- main.py
+++ main.py
@@ -55,6 +55,11 @@ def Main():
     Menu_list = ["1. 교시 입력", "2. 시간(시 분) 입력"]
     Answer = Get_integer_Answer(Question, Menu_list, 2)
 
+    if Answer == 1:
+        pass
+    else:
+        pass
+
     # 다시 이름을 받을 것인지 묻는 부분
 
 
