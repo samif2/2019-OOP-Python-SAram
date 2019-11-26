4e7d55b166d10a8474f3497d1862533693d95dc6
Tochma <tochmalia@gmail.com>
2019-11-26 11:13:58 +0900

    main 함수 작업중: 시간 입력 make: Get_integer_Answer 함수

--- main.py
+++ main.py
@@ -9,7 +9,24 @@ import SortingTime_LSI
 
 
 def Get_integer_Answer(Question, menu_list, range_num):
-    pass
+
+    print(Question)
+
+    for menu in menu_list:
+        print(menu)
+    while True:
+        Answer = input('>')
+
+        try:
+            Answer = int(Answer)
+        except:
+            print("잘못된 입력입니다. 다시 입력하세요.")
+            continue
+
+        if 0 < Answer <= range_num:
+            return Answer
+
+        print("잘못된 입력입니다. 다시 입력하세요.")
 
 
 print("Welcome! to our Program")
