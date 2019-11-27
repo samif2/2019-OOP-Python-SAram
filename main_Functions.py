d158aed516d8180bb7470777bceab5ffa01fbeb3
Tochma <tochmalia@gmail.com>
2019-11-27 12:32:38 +0900

    Get_integer_function 매개변수 수정 + 설명 추가

--- main_Functions.py
+++ main_Functions.py
@@ -1,9 +1,19 @@
-def Get_integer_Answer(Question, menu_list, range_num):
+def Get_integer_Answer(Question, menu_list):
+    """
+    :param Question: 큰 질문. string 타입
+    :param menu_list: 각각의 보기를 담고 있는 리스트.
+    예시: ["먹기", "자기"]
+    :return: 사용자가 답한 입력을 반환 (int 형)
+    """
     print(Question)
     print("0을 누르면 이 질문을 취소합니다.")
 
+    i = 1
+    range_num = len(menu_list)
     for menu in menu_list:
-        print(menu)
+        print(i, ": ", menu)
+        i += 1
+
     while True:
         Answer = input('>')
 
@@ -21,5 +31,9 @@ def Get_integer_Answer(Question, menu_list, range_num):
         print("잘못된 입력입니다. 다시 입력하세요.")
 
 
-def Get_Answers_for_Student(Subject_list):
-    pass
+def Get_Answers_for_Student(Student_name, Subject_list):
+    print("=" * 20)
+    print("학생" + Student_name + "를 찾고 있습니다.")
+
+    Question = "어떤 작업을 하시겠습니까?"
+    Answers = ["추측한 모든 과목 보기", "반 정보 입력", "전공 및 부전공 입력", "듣지 않을 만한 수업 선택"]
