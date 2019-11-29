48670e0fb19330fab9f949cc07479a174dce420f
Tochma <tochmalia@gmail.com>
2019-11-29 19:57:51 +0900

    main_Function: 대략적 뼈대 완성

--- main_Functions.py
+++ main_Functions.py
@@ -32,12 +32,28 @@ def Get_integer_Answer(Question, menu_list):
 
 
 def Get_Class_info():
-    pass
+    print("반을 입력하세요.")
+    while True:
+        Answer = input('>')
+
+        try:
+            Answer = int(Answer)
+        except:
+            print("잘못된 입력입니다. 다시 입력하세요.")
+            continue
+
+        if 1 <= Answer <= 6:
+            return Answer
+
+        print("잘못된 입력입니다. 다시 입력하세요.")
+
 
 
 def Get_Answers_for_Student(Student_name, Subject_list):
     print("=" * 20)
     print("학생 '" + Student_name + "' 찾는 중")
+    Student_class = 0
+    Student_sub = []
 
     while True:
         Question = "어떤 작업을 하시겠습니까?"
@@ -47,15 +63,31 @@ def Get_Answers_for_Student(Student_name, Subject_list):
         if Answer == 0:
             print("학생 찾기를 종료합니다.")
             return
-        elif Answer == 1:
-            print("-" * 20)
+
+        print("-" * 20)
+        if Answer == 1:
+
             print("가능성 있는 과목:")
             for Subject in Subject_list:
                 print(Subject)
-            print("-" * 20)
         elif Answer == 2:
-            pass
+            if Student_class != 0:
+                Student_class = Get_Class_info()
+            else:
+                print("이미 정보를 가지고 있습니다.")
+                print("반:", Student_class)
         elif Answer == 3:
-            pass
+            Question = "전공 및 부전공을 입력합니다."
+            Answers = ["수학", "물리", "화학", "생명과학", "정보과학", "지구과학"]
+            A = Get_integer_Answer(Question, Answers)
+            if A == 6:
+                A = 9
+            else:
+                A = -(A-1)
+            Student_sub.append(A)
         elif Answer == 4:
-            pass
+            Question = "듣지 않을 만한 과목을 선택하세요."
+            A = Get_integer_Answer(Question, Subject_list)
+            del Subject_list[A-1]
+
+        print("-" * 20)
