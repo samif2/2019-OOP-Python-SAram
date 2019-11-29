c3dc8451a18d4cedd8ba2439b8967bc0feaa6682
Tochma <tochmalia@gmail.com>
2019-11-29 23:04:31 +0900

    시간 입력 부분 작업중

--- main.py
+++ main.py
@@ -48,7 +48,6 @@ def Main():
         except:
             print("잘못된 입력입니다.")
 
-
     Question = "찾고자 하는 시간의 형식을 선택하세요"
     Menu_list = ["교시 입력", "시간(시 분) 입력"]
     Answer = Get_integer_Answer(Question, Menu_list)
@@ -64,7 +63,9 @@ def Main():
                 Answer = int(Answer)
                 if 1 <= Answer <= 12:
                     if Answer == 12:
-                        Answer = 1
+                        Answer = 11
+                    SortingTime_LSI.Sort_Time(year=date_year, month=date_month, day=date_day,
+                                              time_name=Answer)
                     break
                 else:
                     print("잘못된 입력입니다.")
